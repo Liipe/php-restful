3d92e753f7c1db73da3ecfddaa39bd873eff22f9
Edy Segura <edysegura@gmail.com>
2015-06-13 17:38:31 +0000

    Adding servides to API

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
+require 'vendor/autoload.php';
 
+$app = new \Slim\Slim();
+
+$app->get('/', function() {
+	echo "Welcome to Guest API";
+});
+
+$app->get('/guests', function() use ( $app ) {
+	$guests = array(
+		array('id' => 1, 'name' => 'Edy Segura', 'email' => 'edysegura@gmail.com'),
+		array('id' => 2, 'name' => 'Rodrigo Faria', 'email' => 'rll@gmail.com')
+	);
+	
+	$app->response()->header('Content-Type', 'application/json');
+	echo json_encode($guests);
+});
+	
+$app->run();
 ?>
